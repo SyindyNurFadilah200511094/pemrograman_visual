25913dd890b139be4ff58051fd394a59f4ca637a
SyindyNurFadilah200511094 <103364259+SyindyNurFadilah200511094@users.noreply.github.com>
2023-07-20 22:41:40 +0700

    Add files via upload

---
+++ PROJECT AKHIR/Aplikasi Baju Pesta/Bajupesta.vb
@@ -0,0 +1,9 @@
+﻿Public Class Bajupesta
+    Public Property nsp As String
+    Public Property nama As String
+    Public Property jns_kostum As String
+    Public Property tgl_sewa As String
+    Public Property ukuran As String
+    Public Property harga As String
+
+End Class
