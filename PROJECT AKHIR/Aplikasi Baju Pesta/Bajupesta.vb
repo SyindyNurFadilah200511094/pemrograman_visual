2bc9202314c337d3abaea060644a19bc0cf50737
SyindyNurFadilah200511094 <103364259+SyindyNurFadilah200511094@users.noreply.github.com>
2023-07-21 19:45:54 +0700

    Delete PROJECT AKHIR directory

--- PROJECT AKHIR/Aplikasi Baju Pesta/Bajupesta.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Bajupesta
-    Public Property nsp As String
-    Public Property nama As String
-    Public Property jns_kostum As String
-    Public Property tgl_sewa As String
-    Public Property ukuran As String
-    Public Property harga As String
-
-End Class
--- PROJECT AKHIR/Aplikasi Baju Pesta/MyMod.vb
+++
@@ -1,7 +0,0 @@
-﻿Module MyMod
-    Public api_folder As String = "appakademik"
-    Public dokter_api As String = "http://f0837623.xsph.ru/" & api_folder & "/dokter_api.php"
-    Public dokter_baru As Boolean
-    Public pasien_api As String = "http://f0837623.xsph.ru/" & api_folder & "/pasien_api.php"
-    Public pasien_baru As Boolean
-End Module
